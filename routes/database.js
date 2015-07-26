d4b256a212cb8ec5d471d204905b1cc0e3b06fe5
Alan Joseph <alan.joseph@myntra.com>
2015-07-26 20:39:30 +0530

    Bug fix

--- routes/database.js
+++ routes/database.js
@@ -5,14 +5,14 @@ var router = express.Router();
 var request = require('request');
 var common = require('../services/common.js');
 
-router.get(['/database', '/database/*'], function(req, res, next) {
+router.get('/_utils/*', function(req, res, next) {
     if(!common.isAdmin(req.session.user && req.session.user.email)) return next();
     req.pipe(request(getUrl(req.url))).pipe(res);
 });
 
 function getUrl(url) {
     //Path.join is not working with request.pipe. So this hack for removing the extra / in the secrets.db
-    return secrets.db.replace(/\/$/, '') + url.replace(/^\/database/, '');
+    return secrets.db.replace(/\/$/, '') + url;
 }
 
 module.exports = router;
