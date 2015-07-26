d74bf7be573b700ca334198f60cde41479ccc7fa
Alan Joseph <alan.joseph@myntra.com>
2015-07-26 20:55:50 +0530

    Bug fix

--- routes/database.js
+++ routes/database.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var request = require('request');
 var common = require('../services/common.js');
 
-router.all(['/database', '/database/*'], function(req, res, next) {
+router.all(['/_utils', '/_utils/*'], function(req, res, next) {
     if(!common.isAdmin(req.session.user && req.session.user.email)) return next();
     req.pipe(request(getUrl(req.url))).pipe(res);
 });
