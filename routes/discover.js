496fed6d83706fa53ed66ce239f4b4271be5ce48
Alan Joseph <alan.joseph@myntra.com>
2015-08-07 01:08:35 +0530

    Changes for displaying the ip and port for SSH

--- routes/discover.js
+++ routes/discover.js
@@ -13,7 +13,7 @@ router.get('/discover/server/:servername', function(req, res, next) {
 	db.read('qa', function(err, body) {
 		if(err) return next();
 		body.serverList = getServerList(body.app);
-		body.sshIpMap = getSshIpPort(body.app);
+		body.sshIpMap = (body.compose_status === 0) ? getSshIpPort(body.app) : {};
 		body.domainName = secrets.mainhost;
 		body.common = common.renderData(req);
 		res.render('discoverserver', body);
