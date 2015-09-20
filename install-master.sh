4a941fa5ea5b8c85b1400fdb2ad9b9092537ca5b
Alan Joseph <alanjosephmec@gmail.com>
2015-09-20 13:53:53 +0530

    Update for building the elb and dockerbox image while installation

--- install-master.sh
+++ install-master.sh
@@ -13,12 +13,20 @@ do_master_init() {
     #Install docker compose
     curl -L https://github.com/docker/compose/releases/download/1.4.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
-    mkdir -p /opt/docker-compose
-    rm -rf /opt/docker-compose/*
+    mkdir -p /opt/dockerboxsetup
+    rm -rf /opt/dockerboxsetup/*
+    mkdir /opt/dockerboxsetup/dockerbox
+    mkdir /opt/dockerboxsetup/elb
     service docker stop
     killall -9 docker
     docker daemon -H tcp://0.0.0.0:2375&
     sleep 20
-    cd /opt/docker-compose/ && wget https://raw.githubusercontent.com/dockerx/dockerbox/admin/Dockerfile&& wget https://raw.githubusercontent.com/dockerx/dockerbox/admin/docker-compose.yml  && DOCKER_HOST=tcp://0.0.0.0:2375 /usr/local/bin/docker-compose up -d
+    cd /opt/dockerboxsetup/dockerbox \
+    && wget https://raw.githubusercontent.com/dockerx/dockerbox/ssi/Dockerfile \
+    cd /opt/dockerboxsetup/elb \
+    && wget https://raw.githubusercontent.com/dockerx/dockerbox-proxy/master/Dockerfile \
+    cd /opt/dockerboxsetup \
+    && wget https://raw.githubusercontent.com/dockerx/dockerbox/ssi/docker-compose.yml \
+    && DOCKER_HOST=tcp://0.0.0.0:2375 /usr/local/bin/docker-compose up -d
 }
 do_master_init
